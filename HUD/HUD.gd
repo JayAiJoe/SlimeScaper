3f307ea4b2b9ab11660124a0af42239411f91b99
Ishbenhuben <gmarksantos@gmail.com>
2023-05-10 23:27:44 +0800

    win screen

---
+++ HUD/HUD.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+var winning_points = 10
+
+func _ready():
+	Events.connect("points_gained", update_score)
+	
+func update_score(player_color, new_score):
+	$ScoreContainer.get_node(player_color+"_score").set_text(str(new_score))
+	if new_score == winning_points:
+		$Winner.set_modulate(GameData.COLORS[player_color])
+		$Winner.set_text(player_color+" wins!")
