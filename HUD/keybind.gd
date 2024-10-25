dc712616d1e3d3350b502a82ed7bd84c306e3d59
Ishbenhuben <gmarksantos@gmail.com>
2024-10-25 14:09:06 +0800

    each key mapped to 1 move only

--- HUD/keybind.gd
+++ HUD/keybind.gd
@@ -3,18 +3,19 @@ extends TextureButton
 @export var action_name : String
 
 signal listening_changed(key:TextureButton, listening:bool)
+signal request_action_change(key:TextureButton, new_action:InputEventKey)
 
 var is_listening := false
-var current_key : InputEventKey
-
+var displayed_key := ""
 
 func _ready() -> void:
 	set_listening(false)
 	var events = InputMap.action_get_events(action_name)
 	if events.size() > 0:
-		current_key = events[0] as InputEventKey
-		var keycode = DisplayServer.keyboard_get_keycode_from_physical(current_key.physical_keycode)
-		$KeyLabel.set_text(OS.get_keycode_string(keycode))
+		var key = events[0] as InputEventKey
+		var keycode = DisplayServer.keyboard_get_keycode_from_physical(key.physical_keycode)
+		displayed_key = OS.get_keycode_string(keycode)
+		$KeyLabel.set_text(displayed_key)
 	
 func _on_button_up() -> void:
 	set_listening(not is_listening)
@@ -27,14 +28,15 @@ func set_listening(val:bool) -> void:
 
 func _input(event) -> void:
 	if event is InputEventKey and event.is_pressed:
-		change_action_input(event)
+		request_action_change.emit(self, event)
 		accept_event()
 
 func change_action_input(input_key : InputEventKey) -> void:
 	if action_name == "":
 		return
-	current_key = input_key
 	InputMap.action_erase_events(action_name)
-	InputMap.action_add_event(action_name, current_key)
-	$KeyLabel.set_text(OS.get_keycode_string(current_key.keycode))
+	InputMap.action_add_event(action_name, input_key)
+	displayed_key = OS.get_keycode_string(input_key.keycode)
+	$KeyLabel.set_text(displayed_key)
+	
 	set_listening(false)
--- HUD/keybind_manager.gd
+++ HUD/keybind_manager.gd
@@ -1,11 +1,22 @@
 extends Control
 
-var current_keys := {}
+var bound_keys := {}
 var only_listener : TextureButton = null
 
 func _ready() -> void:
 	for key in get_children():
 		key.listening_changed.connect(update_listener)
+		key.request_action_change.connect(update_action)
+		bound_keys[key.displayed_key] = key
+
+func update_action(key, action) -> void:
+	var new_string = OS.get_keycode_string(action.keycode)
+	if new_string in bound_keys:
+		return
+	var old_string = bound_keys.find_key(key)
+	bound_keys.erase(old_string)
+	bound_keys[new_string] = key
+	key.change_action_input(action)
 
 func update_listener(key, listening:bool) -> void:
 	if listening and only_listener != null:
